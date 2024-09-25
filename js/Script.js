ca2b0e5d44b892b5fc29fd8bed8ffe29a8caede5
Pedro <pedroliveiralima21@gmail.com>
2024-09-25 16:11:49 -0300

    fix: solutionsolving bug when clicking on the link and the mobile menu does not close

--- js/Script.js
+++ js/Script.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const linksContainer = document.getElementById("links-container");
+  const links = document.querySelectorAll("a");
 
   hamburger.addEventListener("click", function () {
     linksContainer.classList.toggle("show");
+  });
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function () {
+      linksContainer.classList.remove("show");
+    });
   });
 });
